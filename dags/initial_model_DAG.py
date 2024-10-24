1a59f08f09a6f884c823e20be7137607da5ad74a
ENate <nathaniel@aims.ac.za>
2024-10-24 13:20:05 +0200

    Updated initial DAG

--- dags/initial_model_DAG.py
+++ dags/initial_model_DAG.py
@@ -19,7 +19,9 @@ BATCH_SIZE = 128
 NUM_CLASSES = 10
 EPOCHS = 4
 local_tz = pendulum.timezone("Europe/Amsterdam")
-args = {
+with DAG (
+    "initial_model_DAG",
+    default_args = {
     'owner': 'airflow',
     # this in combination with catchup=False ensures the
     # DAG being triggered from the
@@ -29,35 +31,31 @@ args = {
     # this is set to True as we want to pass variables on
     # from one task to another
     'provide_context': True,
-}
-dag = DAG(
-    dag_id='initial_model_DAG',
-    default_args=args,
+    
+    # dag_id='initial_model_DAG',
+    # default_args=args,
     # set interval
-    schedule_interval= '@once',
+    },
+    description= "Initial DAG",
+    schedule = "@once",
     # indicate whether or not Airflow should do any runs for
     # intervals between the start_date and the current date that
     # haven't been run thus far
 	catchup=False
-)
-
-
-task1 = PythonOperator(
+) as dag:
+    task1 = PythonOperator(
     task_id='load_preprocess',
     python_callable=load_preprocess,        # function to be executed
     op_kwargs={'path_stream_sample': PATH_STREAM_SAMPLE,        # input arguments
 			'path_test_set': PATH_TEST_SET},
-    dag=dag,
-)
-
-task2 = PythonOperator(
+    # dag=dag,
+    )
+    task2 = PythonOperator(
     task_id='fit_model',
     python_callable=fit_model,
     op_kwargs={'batch_size': BATCH_SIZE,
             'epochs': EPOCHS,
 			'num_classes': NUM_CLASSES,
             'initial_model_path': INITIAL_MODEL_PATH},
-    dag=dag,
-)
-
-task1 >> task2                  # set task priority
+    )
+    task1 >> task2                  # set task priority
